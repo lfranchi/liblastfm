2acb49bf8b5432d87c0e5f15f0dfdd8ef9fbeee9
Max Howell <max@methylblue.com>
2009-06-05 08:43:26 +0100

    Use git-archive to create archives

--- admin/dist.sh
+++ admin/dist.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
+v=`perl -ne '/VERSION\s*=\s*((\d.)*\d)/ and print \$1' $0/../../src/lastfm.pro`
+
 # OSX copies special file attributes to ._files when you tar!
 export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
 export COPYFILE_DISABLE=true
 
-dst=/tmp/liblastfm-$1
+git archive --prefix=liblastfm-$v/ HEAD | bzip2 > liblastfm-$v.tar.bz2
 
-git clean -xfd
-cp -R . $dst || exit 1
-rm -rf $dst/.git $dst/admin/dist.sh $dst/.gitignore
-cd /tmp
-tar cjf liblastfm-$1.tar.bz2 liblastfm-$1
-cd -
-mv $dst.tar.bz2 .
-rm -r $dst
+echo `pwd`/liblastfm-$v.tar.bz2
