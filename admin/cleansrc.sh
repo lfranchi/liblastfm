57d4c695bfdd881d10cceecf14bcfb51fda1a81a
Max Howell <max@methylblue.com>
2009-06-05 09:44:24 +0100

    Fix cleansrc
    Also add remove trailing spaces line, although I disabled it, as every
    file is touched and they just creep back in anyway.
    
    Would be nice to have an ensure trailing newline command though.

--- admin/cleansrc.sh
+++ admin/cleansrc.sh
@@ -2,7 +2,8 @@
 
 d=`dirname $0`
 
-for x in `$d/findsrc cpp``$d/findsrc h`
+for x in `$d/findsrc cpp` `$d/findsrc h`
 do
     tab2space -unix $x $x
+    #(echo ',s/ *$//'; echo 'wq') | ed -s $x
 done
