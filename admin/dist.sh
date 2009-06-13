09dc6cb4e2612ddf271b254f5824c534a4f54617
Max Howell <max@methylblue.com>
2009-06-13 12:45:26 +0100

    dist.sh improvements
    * Keep DRY
    * Separate digits by . not any character

--- admin/dist.sh
+++ admin/dist.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-v=`perl -ne '/VERSION\s*=\s*((\d.)*\d)/ and print \$1' $0/../../src/lastfm.pro`
+v=`perl -ne '/VERSION\s*=\s*((\d\.)*\d)/ and print \$1' $0/../../src/lastfm.pro`
+basename=liblastfm-$v
 
 # OSX copies special file attributes to ._files when you tar!
 export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
 export COPYFILE_DISABLE=true
 
-git archive --prefix=liblastfm-$v/ HEAD | bzip2 > liblastfm-$v.tar.bz2
+git archive --prefix=$basename/ HEAD | bzip2 > $basename.tar.bz2
 
-echo `pwd`/liblastfm-$v.tar.bz2
+echo `pwd`/$basename.tar.bz2
