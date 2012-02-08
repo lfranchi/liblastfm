35e79c36a7c905a759157d94811de3e0e8edc21b
Michael Coffey <michaelc@last.fm>
2012-02-08 18:38:52 +0000

    Only add a content type if one has not already been set. We set it to something different when bootstrap.

--- src/NetworkAccessManager.cpp
+++ src/NetworkAccessManager.cpp
@@ -136,7 +136,9 @@ lastfm::NetworkAccessManager::createRequest( Operation op, const QNetworkRequest
 
     request.setAttribute( QNetworkRequest::CacheLoadControlAttribute, QNetworkRequest::PreferCache );
     request.setRawHeader( "User-Agent", lastfm::UserAgent );
-    request.setRawHeader( "content-type", "application/x-www-form-urlencoded" );
+
+    if ( request.rawHeader( "Content-type" ).isEmpty() )
+        request.setRawHeader( "Content-type", "application/x-www-form-urlencoded" );
     
 #ifdef WIN32
     // PAC proxies can vary by domain, so we have to check everytime :(
