f3460f5726660ef83eb056e3bc06dacff1009514
Michael Coffey <michaelc@last.fm>
2012-03-29 18:06:50 +0100

    Do post requests instead of get for both write APIs and ones that may have long data in the request.

--- src/Library.cpp
+++ src/Library.cpp
@@ -39,7 +39,7 @@ lastfm::Library::addAlbum( const QList<lastfm::Album>& albums )
         map["album[" + QString::number(i) + "]"] = albums[i].title();
     }
 
-    return ws::get( map );
+    return ws::post( map );
 }
 
 QNetworkReply*
@@ -51,7 +51,7 @@ lastfm::Library::addArtist( const QList<lastfm::Artist>& artists )
     for ( int i = 0 ; i < artists.count() ; ++i )
         map["artist[" + QString::number(i) + "]"] = artists[i].name();
 
-    return ws::get( map );
+    return ws::post( map );
 }
 
 QNetworkReply*
@@ -61,7 +61,7 @@ lastfm::Library::addTrack( const Track &track )
     map["method"] = "library.addTrack";
     map["track"] = track.title();
     map["artist"] = track.artist().name();
-    return ws::get( map );
+    return ws::post( map );
 }
 
 QNetworkReply*
@@ -73,7 +73,7 @@ lastfm::Library::getAlbums( const QString& user, const lastfm::Artist& artist, i
     map["artist"] = artist.name();
     if ( page != -1 ) map["page"] = QString::number( page );
     if ( limit != -1 ) map["limit"] = QString::number( limit );
-    return ws::get( map );
+    return ws::post( map );
 }
 
 QNetworkReply*
@@ -84,7 +84,7 @@ lastfm::Library::getArtists( const QString& user, int limit, int page )
     map["user"] = user;
     if ( page != -1 ) map["page"] = QString::number( page );
     if ( limit != -1 ) map["limit"] = QString::number( limit );
-    return ws::get( map );
+    return ws::post( map );
 }
 
 QNetworkReply*
@@ -96,7 +96,7 @@ lastfm::Library::getTracks( const QString& user, const lastfm::Artist& artist, i
     map["artist"] = artist.name();
     map["limit"] = QString::number( limit );
     map["page"] = QString::number( page );
-    return ws::get( map );
+    return ws::post( map );
 }
 
 QNetworkReply*
@@ -109,7 +109,7 @@ lastfm::Library::getTracks( const QString& user, const lastfm::Album& album, int
     map["artist"] = album.artist().name();
     if ( page != -1 ) map["page"] = QString::number( page );
     if ( limit != -1 ) map["limit"] = QString::number( limit );
-    return ws::get( map );
+    return ws::post( map );
 }
 
 QNetworkReply*
@@ -119,7 +119,7 @@ lastfm::Library::removeAlbum( const lastfm::Album& album )
     map["method"] = "library.removeAlbum";
     map["album"] = album.title();
     map["artist"] = album.artist().name();
-    return ws::get( map );
+    return ws::post( map );
 }
 
 QNetworkReply*
@@ -128,7 +128,7 @@ lastfm::Library::removeArtist( const lastfm::Artist& artist )
     QMap<QString, QString> map;
     map["method"] = "library.removeArtist";
     map["artist"] = artist.name();
-    return ws::get( map );
+    return ws::post( map );
 }
 
 QNetworkReply*
@@ -138,7 +138,7 @@ lastfm::Library::removeTrack( const lastfm::Track& track )
     map["method"] = "library.removeTrack";
     map["artist"] = track.artist().name();
     map["track"] = track.title();
-    return ws::get( map );
+    return ws::post( map );
 }
 
 QNetworkReply*
@@ -149,6 +149,6 @@ lastfm::Library::removeScrobble( const lastfm::Track& track )
     map["artist"] = track.artist().name();
     map["track"] = track.title();
     map["timestamp"] = QString::number( track.timestamp().toTime_t() );
-    return ws::get( map );
+    return ws::post( map );
 }
 
